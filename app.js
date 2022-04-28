1b4095b83beb263418c85c3233320fd4e8b7f1c9
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-27 23:32:18 -0500

    agregado get para HTTP METHODS

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
+//HTTP METHODS
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL request ${new Date()}`)
+    const explorer1 = {id:1, name:"Ubaldo1"}
+    const explorer2 = {id:2, name:"Ubaldo2"}
+    const explorer3 = {id:3, name:"Ubaldo3"}
+    const explorer4 = {id:4, name:"Ubaldo4"}
+    const explorers = [explorer1,explorer2,explorer3,explorer4]
+    res.status(200).json(explorers)
+    // [{"id":1,"name":"Ubaldo1"},{"id":2,"name":"Ubaldo2"},{"id":3,"name":"Ubaldo3"},{"id":4,"name":"Ubaldo4"}]
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
